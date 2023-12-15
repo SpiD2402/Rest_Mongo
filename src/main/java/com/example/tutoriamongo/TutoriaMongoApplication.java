ecf3bfd6dc4d503e96b51bb1f2b979d5f95cc6ae
SpiD2402 <piero1235545@gmail.com>
2023-12-14 21:24:47 -0500

    Principal

---
+++ src/main/java/com/example/tutoriamongo/TutoriaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutoriamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriaMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoriaMongoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/tutoriamongo/documents/Usuario.java
@@ -0,0 +1,15 @@
+package com.example.tutoriamongo.documents;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Usuario {
+
+    @Id
+
+
+
+}
